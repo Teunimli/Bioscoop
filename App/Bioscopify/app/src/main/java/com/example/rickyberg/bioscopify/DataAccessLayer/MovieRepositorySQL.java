331e924c1289b22c807511ad1941de226271869c
KGerretsen <33416856+kgerretsen@users.noreply.github.com>
2018-03-28 11:52:18 +0200

    Update: code verder uitgewerkt MovieRepositorySQL

--- App/Bioscopify/app/src/main/java/com/example/rickyberg/bioscopify/DataAccessLayer/MovieRepositorySQL.java
+++ App/Bioscopify/app/src/main/java/com/example/rickyberg/bioscopify/DataAccessLayer/MovieRepositorySQL.java
@@ -34,12 +34,20 @@ public class MovieRepositorySQL implements MovieRepositoryInterface {
 
         int id = Integer.parseInt(cursor.getString(cursor.getColumnIndex("id")));
         String title = cursor.getString(cursor.getColumnIndex("title"));
-        int adultInt = Integer.parseInt(cursor.getString(cursor.getColumnIndex("adult")));
-        String genre= cursor.getString(cursor.getColumnIndex("genre"));
         String language= cursor.getString(cursor.getColumnIndex("language"));
         String posterpath = cursor.getString(cursor.getColumnIndex("posterpath"));
         String overview = cursor.getString(cursor.getColumnIndex("overview"));
 
+        String genre = cursor.getString(cursor.getColumnIndex("genre"));
+        String[] genreParts = genre.split("");
+
+        ArrayList<String> genreList = new ArrayList<>();
+
+        for (int i = 0; i > genreParts.length; i++){
+            genreList.add(genreParts[i]);
+        }
+
+        int adultInt = Integer.parseInt(cursor.getString(cursor.getColumnIndex("adult")));
         boolean adult;
 
         if (adultInt == 1){
@@ -49,7 +57,8 @@ public class MovieRepositorySQL implements MovieRepositoryInterface {
             adult = false;
         }
 
-        Movie movie = new Movie(id, title, adult, genre, language, posterpath, overview);
+
+        Movie movie = new Movie(id, title, adult, genreList, language, posterpath, overview);
 
         cursor.close();
         db.close(); //Hier db.close of de sqlhandler.closeConnection? of beide?
@@ -70,12 +79,21 @@ public class MovieRepositorySQL implements MovieRepositoryInterface {
         while(cursor.getPosition() < cursor.getCount()) {
             int id = Integer.parseInt(cursor.getString(cursor.getColumnIndex("id")));
             String title = cursor.getString(cursor.getColumnIndex("title"));
-            int adultInt = Integer.parseInt(cursor.getString(cursor.getColumnIndex("adult")));
-            String genre= cursor.getString(cursor.getColumnIndex("genre"));
             String language= cursor.getString(cursor.getColumnIndex("language"));
             String posterpath = cursor.getString(cursor.getColumnIndex("posterpath"));
             String overview = cursor.getString(cursor.getColumnIndex("overview"));
 
+
+            String genre = cursor.getString(cursor.getColumnIndex("genre"));
+            String[] genreParts = genre.split("");
+
+            ArrayList<String> genreList = new ArrayList<>();
+
+            for (int i = 0; i < genreParts.length; i++){
+                genreList.add(genreParts[i]);
+            }
+
+            int adultInt = Integer.parseInt(cursor.getString(cursor.getColumnIndex("adult")));
             boolean adult;
 
             if (adultInt == 1){
@@ -85,7 +103,7 @@ public class MovieRepositorySQL implements MovieRepositoryInterface {
                 adult = false;
             }
 
-            Movies.add(new Movie(id, title, adult, genre, language, posterpath, overview));
+            Movies.add(new Movie(id, title, adult, genreList, language, posterpath, overview));
             cursor.moveToNext();
         }
 
@@ -97,18 +115,23 @@ public class MovieRepositorySQL implements MovieRepositoryInterface {
     @Override
     public boolean addMovie(Movie movie) {
         try {
+            int adult = 0;
+
+            if (movie.getAge()==true){
+                adult = 1;
+            }
+
+            String genreString = "";
+
+            for (int i = 0; i < movie.getGenre().size(); i++){
+                genreString+= movie.getGenre().get(i);
+            }
+
             ContentValues values = new ContentValues();
             values.put("id", movie.getId());
             values.put("title", movie.getTitle());
-
-            if (movie.getAge() == true) {
-                values.put("adult", 1);
-            }
-            else {
-                values.put("adult", 0);
-            }
-
-            values.put("genre", movie.getGenre());
+            values.put("adult", adult);
+            values.put("genre", genreString);
             values.put("language", movie.getLanguage());
             values.put("posterpath", movie.getPosterpath());
             values.put("overview", movie.getOverview());
@@ -124,10 +147,14 @@ public class MovieRepositorySQL implements MovieRepositoryInterface {
     }
 
     @Override
-    public boolean deleteMovie(int id) {
+    public boolean deleteMovie(int movieId) {
         try {
+            String query = "DELETE FROM Movie WHERE id = '" + movieId + "'";
 
+            SQLiteDatabase db = sqlHandler.getWritableDatabase();
+            Cursor cursor = db.rawQuery(query, null);
 
+            db.close();
 
             return true;
         } catch (Exception e) {
