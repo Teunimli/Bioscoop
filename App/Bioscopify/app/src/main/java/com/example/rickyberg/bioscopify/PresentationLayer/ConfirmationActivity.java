04da3058523227e03300780dcd27c1fea9cac91a
nadia <34341952+nkarimi00@users.noreply.github.com>
2018-03-29 16:26:24 +0200

    updated ConfirmActivity

--- App/Bioscopify/app/src/main/java/com/example/rickyberg/bioscopify/PresentationLayer/ConfirmationActivity.java
+++ App/Bioscopify/app/src/main/java/com/example/rickyberg/bioscopify/PresentationLayer/ConfirmationActivity.java
@@ -1,11 +1,20 @@
 package com.example.rickyberg.bioscopify.PresentationLayer;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.example.rickyberg.bioscopify.DomainLayer.Movie;
 import com.example.rickyberg.bioscopify.R;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Date;
 
 public class ConfirmationActivity extends AppCompatActivity {
     private TextView time;
@@ -21,19 +30,57 @@ public class ConfirmationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.confirmation_activity);
-        movie  = (Movie) getIntent().getSerializableExtra("MOVIE");
+        movie = (Movie) getIntent().getSerializableExtra("MOVIE");
 
         time = (TextView) findViewById(R.id.time_tv);
         title = (TextView) findViewById(R.id.movietitle_tv);
-        ticket =(TextView) findViewById(R.id.ticket_tv);
+        ticket = (TextView) findViewById(R.id.ticket_tv);
 
         SEATS = (int) getIntent().getSerializableExtra("SEATS");
         TIME = (String) getIntent().getSerializableExtra("TIME");
 
         title.setText(movie.getTitle());
         time.setText(TIME);
-        ticket.setText(""+SEATS);
+        ticket.setText("" + SEATS);
 
+        takeScreenshot();
+    }
+        private void takeScreenshot() {
+
+            Date now = new Date();
+            android.text.format.DateFormat.format("yyyy-MM-dd_hh:mm:ss", now);
+
+            try {
+                // image naming and path  to include sd card  appending name you choose for file
+                String mPath = Environment.getExternalStorageDirectory().toString() + "/" + now + ".jpg";
+
+                // create bitmap screen capture
+                View v1 = getWindow().getDecorView().getRootView();
+                v1.setDrawingCacheEnabled(true);
+                Bitmap bitmap = Bitmap.createBitmap(v1.getDrawingCache());
+                v1.setDrawingCacheEnabled(false);
+
+                File imageFile = new File(mPath);
+
+                FileOutputStream outputStream = new FileOutputStream(imageFile);
+                int quality = 100;
+                bitmap.compress(Bitmap.CompressFormat.JPEG, quality, outputStream);
+                outputStream.flush();
+                outputStream.close();
+
+                openScreenshot(imageFile);
+            } catch (Throwable e) {
+                // Several error may come out with file handling or DOM
+                e.printStackTrace();
+            }
+        }
+    private void openScreenshot(File imageFile) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        Uri uri = Uri.fromFile(imageFile);
+        intent.setDataAndType(uri, "image/*");
+        startActivity(intent);
+    }
 
     }
-}
+
