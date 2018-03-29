43213c2c82c246a68536d576d4895ef310684e45
KGerretsen <33416856+kgerretsen@users.noreply.github.com>
2018-03-29 10:12:02 +0200

    Update: Ticket select Activity code

--- App/Bioscopify/app/src/main/java/com/example/rickyberg/bioscopify/PresentationLayer/TicketSelectActivity.java
+++ App/Bioscopify/app/src/main/java/com/example/rickyberg/bioscopify/PresentationLayer/TicketSelectActivity.java
@@ -3,38 +3,120 @@ package com.example.rickyberg.bioscopify.PresentationLayer;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.rickyberg.bioscopify.DomainLayer.Movie;
 import com.example.rickyberg.bioscopify.R;
+import com.squareup.picasso.Picasso;
 
 
 public class TicketSelectActivity extends AppCompatActivity {
 
-    private ImageView poster;
-    private TextView title;
-    private EditText juniorTickets;
-    private EditText normalTickets;
-    private EditText seniorTickets;
-    private TextView totalPrice;
-    private Button button;
+    private ImageView posterIv;
+    private TextView titleTv;
+    private EditText juniorTicketsEt;
+    private EditText normalTicketsEt;
+    private EditText seniorTicketsEt;
+    private TextView totalPriceTv;
+    private Button selectSeatsButton;
+    private Movie selectedMovie;
+
+    private double nrOfJuniorTickets;
+    private double nrOfNormalTickets;
+    private double nrOfSeniorTickets;
+    private double totalPrice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        this.poster = (ImageView) findViewById(R.id.imageView);
-        this.title = (TextView) findViewById(R.id.titleTv);
-        this.juniorTickets = (EditText) findViewById(R.id.juniorTicketsEt);
-        this.normalTickets = (EditText) findViewById(R.id.normalTicketsEt);
-        this.seniorTickets = (EditText) findViewById(R.id.seniorTicketsEt);
-        this.totalPrice = (TextView) findViewById(R.id.totalPriceTv);
-        this.button = (Button) findViewById(R.id.selectSeatsBtn); //deze even renamen
+        this.posterIv = (ImageView) findViewById(R.id.imageView);
+        this.titleTv = (TextView) findViewById(R.id.titleTv);
+        this.juniorTicketsEt = (EditText) findViewById(R.id.juniorTicketsEt);
+        this.normalTicketsEt = (EditText) findViewById(R.id.normalTicketsEt);
+        this.seniorTicketsEt = (EditText) findViewById(R.id.seniorTicketsEt);
+        this.totalPriceTv = (TextView) findViewById(R.id.totalPriceTv);
+        this.selectSeatsButton = (Button) findViewById(R.id.selectSeatsBtn);
+
+        Picasso.with(this).load(selectedMovie.getPosterpath()).into(posterIv);
+        titleTv.setText(selectedMovie.getTitle());
+
+
+
+        juniorTicketsEt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                nrOfJuniorTickets = Double.parseDouble(editable.toString());
+                updateTotalPrice();
+            }
+        });
+
+        normalTicketsEt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                nrOfNormalTickets = Double.parseDouble(editable.toString());
+                updateTotalPrice();
+            }
+        });
+
+        seniorTicketsEt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                nrOfSeniorTickets = Double.parseDouble(editable.toString());
+                updateTotalPrice();
+            }
+        });
 
     }
 
+    private void updateTotalPrice(){
+
+        totalPrice = nrOfJuniorTickets * 7.50 + nrOfNormalTickets * 9.00 + nrOfSeniorTickets * 8.50;
+
+        String stringDouble = Double.toString(totalPrice);
+        totalPriceTv.setText(stringDouble);
+    }
+
+    public void onClick(View view)
+    {
+        //SEAT SELECT SCREEN
+    }
 
 }
